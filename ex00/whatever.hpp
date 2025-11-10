9d5163cfba05711c9bb38e7a000b7d3287afd406
Alimat Okhapkina <aokhapki@2-A-2.42heilbronn.de>
2025-11-10 19:00:20 +0100

    add functions in athe headder file, edited Makefile  for ex00

---
+++ ex00/whatever.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 18:28:01 by aokhapki          #+#    #+#             */
+/*   Updated: 2025/11/10 18:29:33 by aokhapki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T min(const T &a, const T &b) {
+    return (a < b) ? a : b;
+}
+
+template <typename T>
+T max(const T &a, const T &b) {
+    return (a > b) ? a : b;
+}
+
+#endif
